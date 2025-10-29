5ed7dab15361928f38d42f8c584cbe9a34639dff
Denis Shilovich <denis.shilovich@mobyrix.com>
2025-10-29 16:11:44 +0300

    fix for Xcode 26

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt", .exact("5.3.0")),
-        .package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/denis15yo/tweetnacl-swiftwrap", branch: "master")
     ],
     targets: [
         .target(
